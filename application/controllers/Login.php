c418e1b60a243158ad11ca346a68c67d26710029
Denisuardi <denisuardi05@gmail.com>
2022-03-25 08:45:44 +0700

    login lanjutan

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,26 +6,11 @@ class Login extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('Login_m', 'login');
+		$this->load->model('Login_m', 'login_model');
 	}
 
 	public function index()
 	{	
-		// $data['title'] = 'Info BAK';
-		// $this->form_validation->set_rules('email', 'Email', 'required',
-        //                 array('required' => 'Masukkan email')
-        //         );
-		// $this->form_validation->set_rules('password', 'Password', 'required',
-		// 		array('required' => 'Masukkan password')
-		// );
-		// if($this->form_validation->run() ==false){
-		// 	$this->load->view('template/auth_header', $data);
-        // 	$this->load->view('v_login');
-        // 	$this->load->view('template/auth_footer');
-		// }else {
-		// 	$post = $this->input->post(null, TRUE);
-		// 	$this->login->login($post);
-		// }
 		$data['title'] = 'Info BAK';
 		$this->load->view('template/auth_header', $data);
 		$this->load->view('v_login');
@@ -35,22 +20,12 @@ class Login extends CI_Controller {
 
 	public function check_login()
 	{
-
-		$email = $this->input->post('email');
-		$password = $this->input->post('password');
-
-		$combine = array('email' => $email, 'password' => $password);
-		$query = $this->db->get_where('admin', $combine);
-		var_dump($query->num_rows());
-		if($query->num_rows() > 0){
-			$row = $query->row();
-			$this->session->set_userdata('login_type','admin');
-			$this->session->set_userdata('admin_login','1');
-			$this->session->set_userdata('admin_id',$row->id);
-			$this->session->set_userdata('login_user_id',$row->id);
-			$this->session->set_userdata('name',$row->name);
-		}else {
-
-		}
+		// $email = html_escape($this->input->post('email'));
+		// $password = html_escape($this->input->post('password'));
+		$post = $this->input->post(null, TRUE);
+		$this->login_model->adminLoginFunction($post);
+		
+		$this->session->set_flashdata('error_mesage','Invalid Login Details');
+		redirect(base_url(). 'login', 'refresh');
 	}
 }
--- application/models/Login_m.php
+++ application/models/Login_m.php
@@ -3,10 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login_m extends CI_Model
 {
-    public function login($post)
+    public function adminLoginFunction($post)
     {
-        $params['email'] = $post['email'];
-        $params['password'] = $post['password'];
+        $combine = array('email' => $post['email'], 'password' => $post['password']);
+		$query = $this->db->get_where('admin', $combine);
+		if($query->num_rows() > 0){
+			$row = $query->row();
+			$this->session->set_userdata('login_type','admin');
+			$this->session->set_userdata('admin_login','1');
+			$this->session->set_userdata('admin_id',$row->id);
+			$this->session->set_userdata('login_user_id',$row->id);
+			$this->session->set_userdata('name',$row->name);
+	
+			$this->session->set_flashdata('flash_mesage', 'Login Successful');
+			redirect(base_url(). 'dashboard');	
+		}
     }
 
     public function register()
